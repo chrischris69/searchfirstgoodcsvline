63ab04e9b8df4592e0ba8dee03252e0f52dd8c57
christian marechal <cmarchal@aol.com>
2018-04-26 14:03:36 +0200

    third commit

--- SearchFirstGoodVanilla.py
+++ SearchFirstGoodVanilla.py
@@ -21,8 +21,11 @@ def readData(fileName):
    f.close()
    return data
 
-csvfile='test.csv'
-csvlignes = readData(csvfile)
+def readData2(fileName):
+   f = open(fileName, "r")
+   data = f.readlines()
+   f.close()
+   return data
 
 #recherche de la premiere regularite dans un ficchier cvs
 #csvlignes = premieres lignes d un fichier csv
@@ -32,6 +35,7 @@ csvlignes = readData(csvfile)
 #Version Vanilla
 import collections
 
+# ne pas appeler directement / subroutine de vanilla2
 def searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon):
     premiereLigne=0
     n = 0
@@ -94,11 +98,84 @@ def searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon):
         
     return (premiereLigne)
 
-limitemax=20
-traceon=True
 
-numero = searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon)
+def searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon):
+    
+    cas0=searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon)
+    if (cas0 > 0):
+        return (cas0)
+    
+    #sinon
+    n = 0     
+    separators = [',',';','\t']
+    df = []    
+    frequence= []
+    
+    # 1) boucle de lecture des premieres lignes
+    for ligne in csvlignes: 
+        tab = []
+        
+        for x in separators:        
+            co = ligne.count(x)
+            tab.append(co)
+
+        frequence.append (tab.index(max(tab)))
+        df.append (tab)
+        
+        n = n + 1
+                
+        if (n >= limitemax):
+            break
+    # 2) Analyse du resultat        
+    
+    mx = max(collections.Counter(frequence)) #separateur
+    nombre = len (frequence)
+    total=0
+    n=0
+    df2 = []
+    
+    for ligne in csvlignes: 
+        tab = []        
+        co = ligne.split (separators [mx])
+        co = [x for x in co if x] #remove empty string
+        a2 = len (co)
+        total = total + a2        
+        df2.append (a2)                
+        n = n + 1                
+        if (n >= limitemax):
+            break
+    moyenne = round (total / nombre) -1
+    premiereLigne=0
+    i=0
+    bfound1=False
+
+    for x in df2:
+        if (bfound1==True): #a minima deux lignes successives
+            if (x >= moyenne):
+                break
+        if (bfound1==False) and (x >= moyenne):
+            premiereLigne=i
+            bfound1=True
+        i= i+1
+
+    if (traceon):
+        print (df)
+        spar=[",", ";","tabulation"]
+        print("Separateur=["+spar[mx]+"], premiere ligne="+str(premiereLigne))
+        
+    return (premiereLigne)
+
+
+limitemax=100
+traceon=False
+
+csvfile='test.csv'
+csvlignes = readData(csvfile)
+
+numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
 print("Premiere ligne->"+str(numero))
 
-numero = searFirstCSVlineGoodVanilla (csvlignes, limitemax, False)
+csvfile='test2.csv'
+csvlignes = readData2(csvfile)
+numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
 print("Premiere ligne->"+str(numero))
