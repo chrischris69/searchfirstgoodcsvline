a0163406d40194316129e83e27ae9e754f9b513a
christian marechal <cmarchal@aol.com>
2018-05-24 09:56:45 +0200

    4 commit

--- SearchFirstGoodVanilla.py
+++ SearchFirstGoodVanilla.py
@@ -10,11 +10,12 @@ correspondante a la regularite d un tableau exploitable
 """
 
 import os
+import csv
+
 path = 'D:/ecomdataforgoodfr/PushMyGIT'
 os.chdir(path)
 
 
-import csv
 def readData(fileName):
    f = open(fileName, "r", encoding="utf-8")
    data = f.readlines()
@@ -32,11 +33,23 @@ def readData2(fileName):
 #limitemax = profondeur maximale de l exploration
 #traceon 
 
+
+def moyenf(tableau):
+    return sum(tableau, 0.0) / len(tableau)
+
+def variance(tableau):
+    m=moyenf(tableau)
+    return moyenf([(x-m)**2 for x in tableau])
+
 #Version Vanilla
 import collections
 
-# ne pas appeler directement / subroutine de vanilla2
-def searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon):
+"""Premiere methode par comptage des separateurs
+   retourne :
+       - le numero de la premiere ligne pour cette methode
+       - le separateur detecte
+"""
+def _searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon):
     premiereLigne=0
     n = 0
      
@@ -64,6 +77,7 @@ def searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon):
     # a-detection du separateur
     
     mx = max(collections.Counter(frequence))
+    nombre = len (frequence)
     
     #la colonne qui nous interesse
     df2 = [row[mx] for row in df]        
@@ -91,45 +105,25 @@ def searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon):
             bfound1=True
         i= i+1
 
-    if (traceon):
+    if (False and traceon):
         print (df)
         spar=[",", ";","tabulation"]
         print("Separateur=["+spar[mx]+"], premiere ligne="+str(premiereLigne))
         
-    return (premiereLigne)
+    return ([premiereLigne, mx, nombre])
 
 
+#Seconde methode, commence par appeler la premiere
 def searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon):
     
-    cas0=searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon)
-    if (cas0 > 0):
-        return (cas0)
-    
-    #sinon
+    #Calcul methode 1
+    result=_searFirstCSVlineGoodVanilla (csvlignes, limitemax, traceon)
+    Metod1= result[0] # numero de la peremiere lgne
+    mx    = result[1] # separateur detecte
+    nombre= result[2] # nombre
     n = 0     
     separators = [',',';','\t']
-    df = []    
-    frequence= []
     
-    # 1) boucle de lecture des premieres lignes
-    for ligne in csvlignes: 
-        tab = []
-        
-        for x in separators:        
-            co = ligne.count(x)
-            tab.append(co)
-
-        frequence.append (tab.index(max(tab)))
-        df.append (tab)
-        
-        n = n + 1
-                
-        if (n >= limitemax):
-            break
-    # 2) Analyse du resultat        
-    
-    mx = max(collections.Counter(frequence)) #separateur
-    nombre = len (frequence)
     total=0
     n=0
     df2 = []
@@ -149,6 +143,12 @@ def searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon):
     i=0
     bfound1=False
 
+    varianceDF2= variance(df2)
+    
+    #print("cas0="+str(cas0))
+    #print("moyenne="+str(moyenne))
+    #print ("variance(df2)="+str(variance(df2)))
+    
     for x in df2:
         if (bfound1==True): #a minima deux lignes successives
             if (x >= moyenne):
@@ -157,25 +157,80 @@ def searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon):
             premiereLigne=i
             bfound1=True
         i= i+1
+     
+    #Decision entre les 2 methodes 
+    first = Metod1
+    MM ="m1"
+    if (varianceDF2 < moyenne): #methode 2
+        first = premiereLigne
+        if (traceon):
+            MM ="m2"
+    else:
+        if Metod1 == 0:
+            first = premiereLigne
+            if (traceon):
+                MM ="m2"
 
-    if (traceon):
-        print (df)
+    if (traceon):        
+        print ("->"+MM)
         spar=[",", ";","tabulation"]
-        print("Separateur=["+spar[mx]+"], premiere ligne="+str(premiereLigne))
-        
-    return (premiereLigne)
+        #print("Separateur=["+spar[mx]+"], premiere ligne="+str(first))
+                    
+    return (first)
 
 
-limitemax=100
-traceon=False
-
-csvfile='test.csv'
-csvlignes = readData(csvfile)
-
-numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
-print("Premiere ligne->"+str(numero))
-
-csvfile='test2.csv'
-csvlignes = readData2(csvfile)
-numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
-print("Premiere ligne->"+str(numero))
+def Testing ():
+    limitemax=100
+    traceon=True
+    
+    csvfile='test.csv' #OK
+    csvlignes = readData(csvfile)
+    
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 4, methode 1,  V>" )
+    
+    csvfile='test2.csv' #OK
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 5, methode 2,  V>" )
+    
+    csvfile='OPENDATA41_Decheteries_TonnagesCollectes_Loir-et-Cher.csv'
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 4, method 2, V <")
+    
+    csvfile='test3.csv' #OK
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 7, method 2, V <")
+    
+    csvfile='test4.csv' #OK
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 0, method 1, V =0")
+    
+    
+    #AVOIR AVOIR AVOIR AVOIR 
+    csvfile='epn_balnode2017022109224716amconmajag7346_2017-12-12.csv'
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 0, method 2, V =0.01 < M")
+    
+    #PAS TRES BON
+    csvfile='D_Tableau 8.csv'
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 0, method 2, V  < M")
+    
+    csvfile='D_Annexe CER 2013.csv'
+    csvlignes = readData2(csvfile)
+    numero = searFirstCSVlineGoodVanilla2 (csvlignes, limitemax, traceon)
+    print("Premiere ligne->"+str(numero))
+    print("BON = 0, method 2, V  < M")
